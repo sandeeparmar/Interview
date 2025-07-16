75887637e5edd29d93f272c1f6fbea3228276d2d
sandeep parmar <23bit062@ietdavv.edu.in>
2025-07-16 14:27:32 +0530

    update names

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar.jsx'
+import Navbar from './components/NavBar.jsx'
 import Footer from './components/Footer.jsx'
 import HomePage from './pages/HomePage.jsx'
 import ResourcesPage from './pages/ResourcesPage.jsx'
